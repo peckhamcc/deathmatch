5d8e291732093e485462cac14f1ef6b57cc03061
achingbrain <alex@achingbrain.net>
2017-10-23 16:21:04 +0100

    Split config into two separate files

--- webpack.config.demo.js
+++ webpack.config.demo.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack')
-const config = require('./webpack.config.js')
+const config = require('./webpack.config.template.js')
 
 config.plugins.push(new webpack.DefinePlugin({
   'process.env': {
