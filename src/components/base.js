c190acf7b9d3f4b0813b898b5039334f6d0913a7
achingbrain <alex@achingbrain.net>
2017-10-27 18:04:54 +0100

    Added graphics

---
+++ src/components/base.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+class Base extends Component {
+
+}
+
+
+const mapStateToProps = ({ game: { trackLength }, players: { players } }) => ({
+  trackLength: trackLength,
+  players: players
+})
+
+const mapDispatchToProps = {
+
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Base)
