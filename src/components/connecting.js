f27697fe11670edec20441e9a020025b10f5d010
achingbrain <alex@achingbrain.net>
2017-10-31 08:41:22 +0000

    Add loading spinner

--- src/components/connecting.js
+++ src/components/connecting.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import styled, { css } from 'styled-components'
+import { DoubleDounceLoading } from 'styled-spinkit'
 
 const ConnectingPanel = styled.div`
-  color: #F10
+  color: #FFF;
+  width: 150px;
+  position: absolute;
+  top: 50px;
+  left: 50%;
+  margin-left: -75px;
+  font-size: 16px;
+  text-align: center;
 `
 
 const Connecting = () => {
   return (
-    <ConnectingPanel>Connecting</ConnectingPanel>
+    <ConnectingPanel>
+      <DoubleDounceLoading color='#FFFFFF' />
+      <p>Connecting</p>
+    </ConnectingPanel>
   )
 }
 
--- src/components/loading-assets.js
+++ src/components/loading-assets.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
+import { DoubleDounceLoading } from 'styled-spinkit'
 
 const LoadingPanel = styled.div`
-  color: #F10
+  color: #FFF;
+  width: 250px;
+  position: absolute;
+  top: 50px;
+  left: 50%;
+  margin-left: -125px;
+  font-size: 16px;
+  text-align: center;
 `
 
 const Loading = ({ loaded }) => {
   return (
-    <LoadingPanel>Loading {loaded}%...</LoadingPanel>
+    <LoadingPanel>
+      <DoubleDounceLoading color='#FFFFFF' />
+      <p>Loading {loaded}%</p>
+    </LoadingPanel>
   )
 }
 
